0f71ba5a87053f5a99d79ce0b8165d008d68d311
kingbhatss <nk.reyes1@gmail.com>
2025-10-17 19:26:28 +0800

    added ISBN scanner and temporary button

--- app/src/main/java/com/mobdeve/s17/group39/itismob_mco/ui/homepage/GoogleBooksApiInterface.kt
+++ app/src/main/java/com/mobdeve/s17/group39/itismob_mco/ui/homepage/GoogleBooksApiInterface.kt
@@ -3,8 +3,16 @@ package com.mobdeve.s17.group39.itismob_mco.ui.homepage
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface GoogleBooksApiInterface {
     @GET("volumes?q=there's+no+freaking+way&maxResults=10&printType=books")
     fun getBooks(): Call<GoogleBooksResponse>
+
+    @GET("volumes")
+    fun searchBooks(
+        @Query("q") query: String,
+        @Query("maxResults") maxResults: Int = 10,
+        @Query("printType") printType: String = "books"
+    ): Call<GoogleBooksResponse>
 }
